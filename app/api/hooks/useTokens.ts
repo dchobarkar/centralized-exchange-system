ae7ecfd25721ae0ea49f6a86c8f300870a5d4888
Darshan Chobarkar <dchobarkar@gmail.com>
2024-08-23 12:52:46 +0530

    initial commit

---
+++ app/api/hooks/useTokens.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import { TokenDetails } from "@/app/lib/tokens";
+
+export interface TokenWithbalance extends TokenDetails {
+  balance: string;
+  usdBalance: string;
+}
+
+export function useTokens(address: string) {
+  const [tokenBalances, setTokenBalances] = useState<{
+    totalBalance: number;
+    tokens: TokenWithbalance[];
+  } | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get(`/api/tokens?address=${address}`).then((res) => {
+      setTokenBalances(res.data);
+      setLoading(false);
+    });
+  }, []);
+
+  return {
+    loading,
+    tokenBalances,
+  };
+}

c6ea90f587606650f1fb9a0d890a4ce341bcc287
Darshan Chobarkar <dchobarkar@gmail.com>
2024-08-23 13:14:11 +0530

    fix components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,39 +1,38 @@
-
 import { getServerSession } from "next-auth";
-import { ProfileCard } from "../components/ProfileCard";
+
 import db from "@/app/db";
+import { ProfileCard } from "../components/ProfileCard";
 import { authConfig } from "../lib/auth";
 
 async function getUserWallet() {
-    const session = await getServerSession(authConfig);
+  const session = await getServerSession(authConfig);
 
-    const userWallet = await db.solWallet.findFirst({
-        where: {
-            userId: session?.user?.uid
-        },
-        select: {
-            publicKey: true
-        }
-    })
+  const userWallet = await db.solWallet.findFirst({
+    where: {
+      userId: session?.user?.uid,
+    },
+    select: {
+      publicKey: true,
+    },
+  });
 
-    if (!userWallet) {
-        return {
-            error: "No solana wallet found associated to the user"
-        }
-    }
-    
-    return {error: null, userWallet};
+  if (!userWallet)
+    return {
+      error: "No solana wallet found associated to the user",
+    };
+
+  return { error: null, userWallet };
 }
 
 export default async function Dashboard() {
-    const userWallet = await getUserWallet();
+  const userWallet = await getUserWallet();
 
-    if (userWallet.error || !userWallet.userWallet?.publicKey) {
-        return <>No solana wallet found</>
-    }
+  if (userWallet.error || !userWallet.userWallet?.publicKey)
+    return <>No solana wallet found</>;
 
-    return <div>
-
-        <ProfileCard publicKey={userWallet.userWallet?.publicKey} />
+  return (
+    <div>
+      <ProfileCard publicKey={userWallet.userWallet?.publicKey} />
     </div>
+  );
 }
--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,94 +1,90 @@
+import { Session } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import db from "@/app/db";
 import { Keypair } from "@solana/web3.js";
 
-import { Session } from 'next-auth';
+import db from "@/app/db";
 
 export interface session extends Session {
-    user: {
-      email: string;
-      name: string;
-      image: string
-      uid: string;
-    };
+  user: {
+    email: string;
+    name: string;
+    image: string;
+    uid: string;
+  };
 }
 
 export const authConfig = {
-    secret: process.env.NEXTAUTH_SECRET || 'secr3t',
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
-        })
-    ],
-    callbacks: {
-        session: ({ session, token }: any): session => {
-            const newSession: session = session as session;
-            if (newSession.user && token.uid) {
-              // @ts-ignore
-              newSession.user.uid = token.uid ?? "";
-            }
-            return newSession!;
+  secret: process.env.NEXTAUTH_SECRET || "secr3t",
+
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+
+  callbacks: {
+    session: ({ session, token }: any): session => {
+      const newSession: session = session as session;
+      if (newSession.user && token.uid) {
+        // @ts-ignore
+        newSession.user.uid = token.uid ?? "";
+      }
+      return newSession!;
+    },
+    async jwt({ token, account, profile }: any) {
+      const user = await db.user.findFirst({
+        where: {
+          sub: account?.providerAccountId ?? "",
         },
-        async jwt({ token, account, profile }: any) {
-            const user = await db.user.findFirst({
-                where: {
-                    sub: account?.providerAccountId ?? ""
-                }
-            })
-            if (user) {
-              token.uid = user.id
-            }
-            return token
-        },
-        async signIn({ user, account, profile, email, credentials }: any) {
-            if (account?.provider === "google") {
-                const email = user.email;
-                if (!email) {
-                    return false
-                }
+      });
+      if (user) token.uid = user.id;
 
-                const userDb = await db.user.findFirst({
-                    where: {
-                        username: email
-                    }
-                })
+      return token;
+    },
+    async signIn({ user, account, profile, email, credentials }: any) {
+      if (account?.provider === "google") {
+        const email = user.email;
+        if (!email) return false;
 
-                if (userDb) {
-                    return true;
-                }
+        const userDb = await db.user.findFirst({
+          where: {
+            username: email,
+          },
+        });
 
-                const keypair = Keypair.generate();
-                const publicKey = keypair.publicKey.toBase58();
-                const privateKey = keypair.secretKey;
+        if (userDb) return true;
 
-                await db.user.create({
-                    data: {
-                        username: email,
-                        name: profile?.name,
-                        //@ts-ignore
-                        profilePicture: profile?.picture,
-                        provider: "Google",
-                        sub: account.providerAccountId,
-                        solWallet: {
-                            create: {
-                                publicKey: publicKey,
-                                privateKey: privateKey.toString()
-                            }
-                        },
-                        inrWallet: {
-                            create: {
-                                balance: 0
-                            }
-                        }
-                    }
-                })
+        const keypair = Keypair.generate();
+        const publicKey = keypair.publicKey.toBase58();
+        const privateKey = keypair.secretKey;
 
-                return true;
+        await db.user.create({
+          data: {
+            username: email,
+            name: profile?.name,
+            //@ts-ignore
+            profilePicture: profile?.picture,
+            provider: "Google",
+            sub: account.providerAccountId,
+            solWallet: {
+              create: {
+                publicKey: publicKey,
+                privateKey: privateKey.toString(),
+              },
+            },
+            inrWallet: {
+              create: {
+                balance: 0,
+              },
+            },
+          },
+        });
 
-            }
-            
-            return false
-        },
-    }
-}
+        return true;
+      }
+
+      return false;
+    },
+  },
+};
